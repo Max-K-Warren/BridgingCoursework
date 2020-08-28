4f8813f0e284f377b233edefb67513c4f88cdce2
max <maxwarren@live.co.uk>
2020-08-28 01:57:54 +0100

    Added __str__s to database models for improved readability in admin window

--- cv/models.py
+++ cv/models.py
@@ -4,8 +4,14 @@ class CV_Category(models.Model):
     name = models.TextField()
     position = models.IntegerField()
 
+    def __str__(self):
+        return self.name
+
 class CV_Item(models.Model):
     name = models.TextField()
     position = models.IntegerField()
     CV_Category = models.ForeignKey(CV_Category, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
 
