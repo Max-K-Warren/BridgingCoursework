9ea294b57782f6b04e3900e5c00ca92f01243809
max <maxwarren@live.co.uk>
2020-08-28 16:40:52 +0100

    added helper functions for tests

--- cv/tests.py
+++ cv/tests.py
@@ -10,21 +10,28 @@ class CVViewTests(TestCase):
 
 class CVInfoModelTests(TestCase):
 
-    def test_do_database_tables_exist(self):
-        CV_Category()
-        CV_Item()
+    def save_category(self, cat_name, cat_position):
+        new_category = CV_Category()
+        new_category.name = cat_name
+        new_category.position = cat_position
+        new_category.save()
+        return new_category
+
+    def save_item(self, item_name, item_description, item_position, item_category):
+        new_item = CV_Item()
+        new_item.name = item_name
+        new_item.description = item_description
+        new_item.position = item_position
+        new_item.CV_Category = item_category
+        new_item.save()
+        return new_item
+
 
     def test_can_make_item_category_structure(self):
-        first_category = CV_Category()
-        first_category.name = "Komi"
-        first_category.position = 1
-        first_category.save()
-        first_item = CV_Item()
-        first_item.name = "communication 1"
-        first_item.description = "totally normal"
-        first_item.position = 1
-        first_item.CV_Category = first_category
-        first_item.save()
+        first_category = self.save_category("Komi", 1)
+        first_item = self.save_item("communication 1", "totally normal", 1, first_category)
         example_dict = CVStructureMaker()
         self.assertEqual(str(example_dict), "[['Komi', [<CV_Item: communication 1>]]]")
+
+    
 
